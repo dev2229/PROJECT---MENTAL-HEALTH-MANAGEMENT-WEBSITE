47c22298cd73fced5ccb450f1df0e42503ca40ae
dev2229 <djahdav@gmail.com>
2024-01-03 12:49:12 +0530

    ADD INITIAL FILES

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("loginButton").addEventListener("click", function() {
+    // Navigate to the login page when the "Login" button is clicked
+    window.location.href = "login.html"; // Replace with the desired URL
+});
